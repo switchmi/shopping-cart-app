a0568d779f82fd519f7e89d675317fc9f7daab0c
fahmi <seichan83@gmail.com>
2017-11-13 16:29:26 +0800

    Modified the application rb file

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ module ShoppingCartApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.filestack_rails.api_key = "AWN4QQZPfSWmFLK7b0UfZz"
+    config.filestack_rails.api_key = ENV["API_KEY"]
     config.filestack_rails.client_name = "filestack_client"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
