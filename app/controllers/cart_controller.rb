8c7a5e97735eb88b9d421ab2b2f05c213ac5b320
fahmi <seichan83@gmail.com>
2017-11-14 17:48:31 +0800

    modified the cart contents

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -6,18 +6,33 @@ class CartController < ApplicationController
   end
 
   def add
+    cart_ids = $redis.hkeys current_user_cart
+    @products = Product.find(cart_ids)
     if $redis.hexists current_user_cart, params[:product_id]
       $redis.hincrby current_user_cart, params[:product_id], 1
     else
       $redis.hset current_user_cart, params[:product_id], 1
     end
     # render json: current_user.cart_count, status: 200
-    redirect_to product_path(params[:product_id])
+    respond_to do |format|
+      format.html {redirect_to product_path(params[:product_id])}
+      format.js
+    end
   end
 
   def remove
-    $redis.hincrby current_user_cart, params[:product_id], -1
-    redirect_to cart_path(current_user.id)
+    cart_ids = $redis.hkeys current_user_cart
+    @products = Product.find(cart_ids)
+    quantity = $redis.hget current_user_cart, params[:product_id]
+    if quantity.to_i == 1
+      $redis.hdel current_user_cart, params[:product_id]
+    else
+      $redis.hincrby current_user_cart, params[:product_id], -1
+    end
+    respond_to do |format|
+      format.html {redirect_to product_path(params[:product_id])}
+      format.js
+    end
   end
 
   private
