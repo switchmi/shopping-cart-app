0c94e9d6d9f6fddf3da7b5cc623c8f13e1979922
fahmi <seichan83@gmail.com>
2017-11-17 02:35:08 +0800

    Added Order Complete Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,10 @@ class UserMailer < ApplicationMailer
     # @url  = 'http://example.com/login'
     mail(to: @user.email, subject: 'Welcome to My Bros Site')
   end
+
+  def order_complete(user)
+    @user = user
+    @orders = user.products
+    mail(to: @user.email, subject: 'Congratulations! You order is complete!', from: 'bro@brobagz.com')
+  end
 end
