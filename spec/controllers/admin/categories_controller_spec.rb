58d5cab706fb0757a533a7d74affb1f1c0c8ff17
jonthtang <jonthtang@gmail.com>
2017-11-16 12:21:48 +0800

    Controller Specs for categories, pages, products (without search)

---
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -0,0 +1,8 @@
+# require 'rails_helper'
+#
+# RSpec.describe Admin::CategoriesController, type: :controller do
+#   describe 'GET #new' do
+#     before { get :new}
+#     it { expect(assigns(:category)).to be_a_new(Category)}
+#   end
+# end
