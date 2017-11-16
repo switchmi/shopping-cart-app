19fd6774cf009c767163f9101ff2e21dc3f93783
fahmi <seichan83@gmail.com>
2017-11-16 23:01:51 +0800

    Added sessions for guests

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,19 +1,33 @@
 class CartController < ApplicationController
-  before_action :authenticate_user!
   def show
-    cart_ids = $redis.hkeys current_user_cart
+    if current_user
+      cart_ids = $redis.hkeys current_user_cart
+    else
+      init_session_cart
+      cart_ids = session[:cart].keys
+    end
     @products = Product.find(cart_ids)
   end
 
   def add
-    cart_ids = $redis.hkeys current_user_cart
-    @products = Product.find(cart_ids)
-    if $redis.hexists current_user_cart, params[:product_id]
-      $redis.hincrby current_user_cart, params[:product_id], 1
+    if current_user
+      cart_ids = $redis.hkeys current_user_cart
+      @products = Product.find(cart_ids)
+      if $redis.hexists current_user_cart, params[:product_id]
+        $redis.hincrby current_user_cart, params[:product_id], 1
+      else
+        $redis.hset current_user_cart, params[:product_id], 1
+      end
     else
-      $redis.hset current_user_cart, params[:product_id], 1
+      cart_ids = session["cart"].keys
+      @products = Product.find(cart_ids)
+      init_session_cart
+      if session["cart"][params[:product_id]]
+        session["cart"][params[:product_id]] += 1
+      else
+        session["cart"][params[:product_id]] = 1
+      end
     end
-    # render json: current_user.cart_count, status: 200
     respond_to do |format|
       format.html {redirect_to product_path(params[:product_id])}
       format.js
@@ -21,13 +35,24 @@ class CartController < ApplicationController
   end
 
   def remove
-    cart_ids = $redis.hkeys current_user_cart
-    @products = Product.find(cart_ids)
-    quantity = $redis.hget current_user_cart, params[:product_id]
-    if quantity.to_i == 1
-      $redis.hdel current_user_cart, params[:product_id]
+    if current_user
+      cart_ids = $redis.hkeys current_user_cart
+      @products = Product.find(cart_ids)
+      quantity = $redis.hget current_user_cart, params[:product_id]
+      if quantity.to_i == 1
+        $redis.hdel current_user_cart, params[:product_id]
+      else
+        $redis.hincrby current_user_cart, params[:product_id], -1
+      end
     else
-      $redis.hincrby current_user_cart, params[:product_id], -1
+      cart_ids = session["cart"].keys
+      @products = Product.find(cart_ids)
+      quantity = session['cart'][params[:product_id]]
+      if quantity == 1
+        session["cart"].delete(params[:product_id])
+      else
+        session["cart"][params[:product_id]] -= 1
+      end
     end
     respond_to do |format|
       format.html {redirect_to product_path(params[:product_id])}
@@ -38,5 +63,11 @@ class CartController < ApplicationController
   private
   def current_user_cart
     "cart#{current_user.id}"
+  end
+
+  def init_session_cart
+    unless session[:cart]
+      session[:cart] = {}
+    end
   end
 end
